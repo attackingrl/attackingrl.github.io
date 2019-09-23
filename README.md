354b362797b7121456b7733b042828419f85e18e
Anonymous <anonymous@review.com>
2019-09-23 12:21:03 -0700

    Update from yarn to 5ebee4f1ff36bf8e63d8c7fdc9764dfc8841cc03

--- README.md
+++
@@ -1,1 +0,0 @@
-# adversarialpolicies.github.io
---
+++ precache-manifest.14b080c9443a3b4e75de39b48ae3d8a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121d621134a56485316061c8fc066507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982fddababb136dc48e7",
+    "url": "/static/css/main.e2d025c6.chunk.css"
+  },
+  {
+    "revision": "09e066796953e3d6cb47",
+    "url": "/static/js/2.4d3e68e2.chunk.js"
+  },
+  {
+    "revision": "982fddababb136dc48e7",
+    "url": "/static/js/main.a5ff4ad1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
